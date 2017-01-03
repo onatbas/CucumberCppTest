c94b52eb2d9ea244133b5c48f49fa174c8dd2fc4
Onat Bas <i@onat.me>
2017-01-03 19:37:49 +0200

    Changes which were suggested in http://stackoverflow.com/questions/41416608/cucumber-cpp-step-defiinition-runner-exits-immediately/41433375#41433375 are applied.

--- cmake/configure_boost.cmake
+++ cmake/configure_boost.cmake
@@ -1,6 +1,14 @@
 set(Boost_USE_STATIC_RUNTIME OFF)
 
-find_package(Boost REQUIRED COMPONENTS thread regex filesystem system unit_test_framework)
+find_package(Boost REQUIRED COMPONENTS
+        thread
+        regex
+        system
+        date_time
+        filesystem
+        chrono
+        program_options
+        unit_test_framework)
 
 if(Boost_FOUND)
  include_directories(${Boost_INCLUDE_DIRS})
